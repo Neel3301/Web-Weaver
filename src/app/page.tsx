b2625f2ba08bd9471786154ace725b87bde83edd
vishalpawar26 <vishalpawar1784@gmail.com>
2024-07-21 16:03:50 +0530

    Build Template Section

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,11 @@
 import Hero from "@/components/Hero";
+import TemplateSection from "@/components/TemplateSection";
 
 export default function Home() {
   return (
     <div className="m-auto flex max-w-[1440px] flex-col px-4 md:px-24 lg:px-36">
       <Hero />
+      <TemplateSection />
     </div>
   );
 }
